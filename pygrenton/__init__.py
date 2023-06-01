ac9583cb6f001e5b4e47c85799275dc7bd62a00d
Rogal <progalin55@gmail.com>
2023-06-01 18:24:27 +0200

    implemented asynchronous request sending

--- pygrenton/__init__.py
+++ pygrenton/__init__.py
@@ -1,3 +1,4 @@
+import asyncio
 import socket
 import threading
 import queue
@@ -13,22 +14,25 @@ class GrentonApi:
         self._cypher = GrentonCypher(key, iv)
 
         self._responses: dict = {}
-        self._msg_queue: queue.Queue[tuple[Request, threading.Condition]] = queue.Queue()
+        self._msg_queue: queue.Queue[tuple[Request, threading.Event]] = queue.Queue()
 
         self._msg_condition = threading.Condition()
         self._msg_sender_thread = threading.Thread(target=self._sender_loop, daemon=True)
         self._msg_sender_thread.start()
 
-    def send_request(self, req: Request):
-        cv = self._appendQueue(req)
+    async def send_request_async(self, req: Request):
+        event = self._appendQueue(req)
 
-        with cv:
-            cv.wait(self._timeout)
-            resp = self._responses.pop(req._id)
-            if resp is None:
-                raise TimeoutError
-            
-            return req.parse_response(resp)
+        event.wait()
+        resp = self._responses.pop(req._id)
+        
+        if isinstance(resp, Exception):
+            raise resp
+        
+        return req.parse_response(resp)
+
+    def send_request(self, req: Request):
+        return asyncio.run(self.send_request_async(req))
 
     def checkAlive(self) -> bool:
         try:
@@ -45,12 +49,12 @@ class GrentonApi:
     
     def _appendQueue(self, req: Request):
         with self._msg_condition:
-            resp_cv = threading.Condition()
+            resp_event = threading.Event()
 
-            self._msg_queue.put((req, resp_cv))
+            self._msg_queue.put((req, resp_event))
             self._msg_condition.notify_all()
 
-            return resp_cv
+            return resp_event
     
     def _sender_loop(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -58,20 +62,21 @@ class GrentonApi:
 
         while True:
             with self._msg_condition:
-                self._msg_condition.wait_for(lambda: not len(self._msg_queue) == 0)
-                req, resp_cv = self._msg_queue.get()
+                self._msg_condition.wait_for(lambda: not self._msg_queue.empty())
+                req, resp_event = self._msg_queue.get()
 
             payload = self._cypher.encrypt(bytes(req.create_request(self._local_ip), "utf-8"))
 
-            sock.sendto(payload, self._addr)
-
             try:
+                sock.sendto(payload, self._addr)
+                
                 resp = sock.recvfrom(1024)[0]
                 resp = self._cypher.decrypt(resp).decode("utf-8")
 
                 self._responses[req._id] = resp
-                resp_cv.notify_all()
 
-            except:
-                pass
+            except Exception as e:
+                self._responses[req._id] = e
+                
+            resp_event.set()
 
