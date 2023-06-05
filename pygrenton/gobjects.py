05e6e18bd9b8bc00717bea3441f7726a594c770d
Rogal <progalin55@gmail.com>
2023-06-06 01:38:19 +0200

    WIP #6

--- pygrenton/gobjects.py
+++ pygrenton/gobjects.py
@@ -1,9 +1,8 @@
 
 from .types import DataType
 
-#TODO: should I make them dataclasses instead of what they are now
-
 class GFeature:
+    clu: GCLU
     name: str
     index: int
     settable: bool
@@ -13,6 +12,17 @@ class GFeature:
 
     enum: list
     value_range: tuple[int, int]
+    
+    def __init__(self, clu, name, index, settable, gettable, data_type, unit, enum=None, value_range=None) -> None:
+        self.clu = clu
+        self.name = name
+        self.index = index
+        self.settable = settable
+        self.gettable = gettable
+        self.data_type = data_type
+        self.unit = unit
+        self.enum = enum
+        self.value_range = value_range
 
     def getValue(self):
         if not self.gettable:
@@ -28,98 +38,112 @@ class GFeature:
         if self.enum is not None and value not in self.enum:
             raise ValueError(f"Value: {value} is not in enum: {self.enum}")
         if self.value_range is not None and (value < self.value_range[0] or value > self.value_range[1]):
-            raise ValueError(f"Value: {value} is not in value range: ({self.value_rang[0]} - {self.value_rang[1]})")
+            raise ValueError(f"Value: {value} is not in value range: ({self.value_range[0]} - {self.value_range[1]})")
         
         #TODO: setting data
 
 class GMethod:
+    clu: GCLU
     name: str
     index: int
 
+    #TODO: implement parameters
     params: list
 
     return_type: DataType
     unit: str
+    
+    def __init__(self, clu, name, index, params, return_type, unit) -> None:
+        self.clu = clu
+        self.name = name
+        self.index = index
+        self.params = params
+        self.return_type = return_type
+        self.unit = unit
 
     def execute(self, **kwargs):
         #TODO: input validation
         #TODO: executing a method
         pass
 
-
 class GObject:
-    _name: str
-    _unique_id: str
-    _obj_type: int
+    clu: GCLU
+    name: str
+    id: str
+    obj_type: int
 
-    _features: list[GFeature]
-    _methods: list[GMethod]
-
-    def __init__(self, name: str, id: str, obj_type: int, features: list, methods: list) -> None:
-        self._name = name
-        self._unique_id = id
-        self._obj_type = obj_type
-        self._features = features
-        self._methods = methods
-        
-    def getFeatures(self) -> list[GFeature]:
-        return self._features
+    features: list[GFeature]
+    methods: list[GMethod]
+    
+    def __init__(self, clu, name, id, obj_type, features, methods) -> None:
+        self.clu = clu
+        self.name = name
+        self.id = id
+        self.obj_type = obj_type
+        self.features = features
+        self.methods = methods
 
     def getFeatureByName(self, name: str) -> GFeature | None:
-        for feature in self._features:
+        for feature in self.features:
             if feature.name == name:
                 return feature
             
         return None
 
     def getFeatureByIndex(self, index: int) -> GFeature | None:
-        for feature in self._features:
+        for feature in self.features:
             if feature.index == index:
                 return feature
             
         return None
 
-    def getMethods(self) -> list[GMethod]:
-        return self._methods
-
     def getMethodByName(self, name: str) -> GMethod | None:
-        for method in self._methods:
+        for method in self.methods:
             if method.name == name:
                 return method
             
         return None
 
     def getMethodByIndex(self, index: int) -> GMethod | None:
-        for method in self._methods:
+        for method in self.methods:
             if method.index == index:
                 return method
             
         return None
+    
+    def getValue(self, index: int):
+        pass
+    
+    def setValue(self, index: int, value):
+        pass
+    
+    def execute(self, index: int, **kwargs):
+        pass
 
 class GModule:
     
     #TODO: add data about module eg. name, hwType, version etc.
     
-    _objects: list[GObject]
+    clu: GCLU
+    gobjects: list[GObject]
     
-    #TODO: add constructor
-    
-    def getObjects(self) -> list[GObject]:
-        return self._objects
+    def __init__(self) -> None:
+        #TODO: just do it.
+        pass
     
     def getObjectsByType(self, obj_type: int) -> list[GObject]:
-        return [obj for obj in self._objects if obj._obj_type == obj_type]
+        return [obj for obj in self.gobjects if obj.obj_type == obj_type]
     
     def getObjectById(self, id: str) -> GObject:
-        for obj in self._objects:
-            if obj._unique_id == id:
+        for obj in self.gobjects:
+            if obj.unique_id == id:
                 return obj
             
         return None
     
     def getObjectByName(self, name: str) -> GObject:
-        for obj in self._objects:
-            if obj._name == name:
+        for obj in self.gobjects:
+            if obj.name == name:
                 return obj
             
         return None
