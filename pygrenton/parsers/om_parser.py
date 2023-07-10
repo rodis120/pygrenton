0db0af51b72d9857457694b44fa10856f12546ba
Rogal <progalin55@gmail.com>
2023-07-10 18:16:31 +0200

    rewritten om_parser

--- pygrenton/parsers/om_parser.py
+++ pygrenton/parsers/om_parser.py
@@ -1,14 +1,37 @@
 
 import re
+import ipaddress
+from ipaddress import IPv4Address
+from dataclasses import dataclass, field
 
 _NAME_FIELD = re.compile("-- NAME_.*")
-_MODULE_FIELD = re.compile("mm_\d{9} = OBJECT:new([^,]*,[^,\n]*){3}\) --.*") #TODO: delete it
-_IO_MODULE_FIELD = re.compile("[A-Z]{3}\d{4}.*OBJECT:new([^,]*,[^,\n]*){3}\)")
-_OBJECT_FIELD = re.compile("[A-Z]{3}\d{4}.*OBJECT:new([^,]*,[^,\n]*){1}\)")
-_EXTRACT_BRACKET = re.compile("(?<=\().*(?=\))")
-_EXTRACT_COMMENT = re.compile("(?<=-- ).*")
-_EXTRACT_NUMBER = re.compile("\d*")
 _SPLIT_ARGS = re.compile("[ =,]*")
+_OBJECT_CREATION = re.compile('/.*[^"]OBJECT:new\(.*?\)[^"\n]*/g')
+
+@dataclass
+class CLU:
+    object_id: str
+    ipaddress: IPv4Address
+
+@dataclass
+class CLUObject:
+    object_id: str
+    object_type: int
+    
+@dataclass
+class ModuleObject(CLUObject):
+    parent_serial_number: int
+    object_index: int
+
+@dataclass
+class OMEndpoints:
+    this_clu: CLU
+    external_clus: list[CLU] = field(default_factory=list)
+    
+    names: dict[str, str] = field(default_factory=dict)
+    
+    clu_objects: list[CLUObject] = field(default_factory=list)
+    module_objects: dict[int, list[ModuleObject]] = field(default_factory=list)
 
 def _parse_name(line: str) -> tuple[str, str]:
     spl = re.split(_SPLIT_ARGS, line)[-2:]
@@ -18,63 +41,59 @@ def _parse_name(line: str) -> tuple[str, str]:
 
     return object_id, name
 
-#TODO: delete this too
-def _parse_module(line: str) -> tuple[int, int, int, int]:
-    bracket = re.search(_EXTRACT_BRACKET, line).string
-    args1 = re.split(_SPLIT_ARGS, bracket)
+def _parse_object_creation(line: str) -> tuple[str, list[str]]:
+    obj_id = line.split(" ", 1)[0]
+    args = line[line.find("(")+1:line.rfind(")")].split(", ")
+    
+    return obj_id, args
 
-    comment = re.search(_EXTRACT_COMMENT, line).string
-    args2 = comment.split(" ")
+def _parse_ip_hex(hex_str: str) -> IPv4Address:
+    return ipaddress.ip_address(int(hex_str, 16))
 
-    sn = int(args2[0])
-    hw_type = int(args1[2], 16)
-    fw_type = int(args2[1])
-    fw_api_version = int(args2[2])
-
-    return sn, hw_type, fw_type, fw_api_version
-
-def _parse_io_module(line: str) -> tuple[str, int, int]:
-    bracket = re.search(_EXTRACT_BRACKET, line).string
-    args = re.split(_SPLIT_ARGS, bracket)
-
-    object_class = int(args[0])
-    module_sn = int(re.search(_EXTRACT_NUMBER, args[1]).string)
-    object_id = args[3][1:-1]
-
-    return object_id, object_class, module_sn
-
-def _parse_object(line: str) -> tuple[str, int]:
-    bracket = re.search(_EXTRACT_BRACKET, line).string
-    args = re.split(_SPLIT_ARGS, bracket)
-
-    object_class = int(args[0])
-    object_id = args[1][1:-1]
-
-    return object_id, object_class
-
-def parse_om(file):
+def parse_om(file) -> OMEndpoints:
     names: dict[str, str] = {}
-    io_modules: dict[int, list[tuple[str, int, int]]] = {}
-    objects: list[tuple[str, int]] = []
-
+    module_objects: dict[int, list[ModuleObject]] = {}
+    clu_objects: list[CLUObject] = []
+    
+    this_clu: CLU
+    external_clus: list[CLU] = []
+            
     for line in file:
         if re.match(_NAME_FIELD, line):
             obj_id, name = _parse_name(line)
             names[obj_id] = name
 
-        elif re.match(_IO_MODULE_FIELD, line):
-            io_mod = _parse_io_module(line)
-            sn = io_mod[2]
+        elif re.match(_OBJECT_CREATION, line):
+            obj_id, args = _parse_object_creation(line)
+            argcount = len(args)
             
-            if io_modules[sn] is not None:
-                io_modules[sn].append(io_mod)
-            else:
-                io_modules[sn] = [io_mod]
-        
-        elif re.match(_OBJECT_FIELD, line):
-            obj = _parse_object(line)
-            objects.append(obj)
+            if argcount == 2:
+                # parse clu object
+                clu_objects.append(CLUObject(obj_id, int(args[0])))
 
-    return names, io_modules, objects
+            elif argcount == 3 :
+                # parse this clu or external clu
+                ip_address = _parse_ip_hex(args[1])
+                
+                if int(args[0]) == 1:
+                    external_clus.append(CLU(obj_id, ip_address))
+                else:
+                    main_clu = CLU(obj_id, ip_address)
+                
+            elif argcount == 4:
+                # parse module object
+                obj_class = int(args[0])
+                if obj_class == 2:
+                    continue
+                
+                parent_serial_number = int(args[1][3:])
+                index = int(args[2])
+                
+                if parent_serial_number in module_objects.keys():
+                    module_objects[parent_serial_number].append(ModuleObject(obj_id, obj_class, parent_serial_number, index))
+                else:
+                    module_objects[parent_serial_number] = [ModuleObject(obj_id, obj_class, parent_serial_number, index)]
+
+    return OMEndpoints(this_clu, external_clus, names, clu_objects, module_objects)
 
                 
