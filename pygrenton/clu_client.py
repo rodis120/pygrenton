40fffc3cc593a52c4d790391c484ce21db26d63d
Rogal <progalin55@gmail.com>
2024-07-09 23:54:27 +0200

    Rewritten client registration.

--- pygrenton/clu_client.py
+++ pygrenton/clu_client.py
@@ -1,7 +1,8 @@
 import asyncio
 import socket
 import threading
-from dataclasses import dataclass
+import time
+from dataclasses import dataclass, field
 from collections.abc import Callable
 from typing import Any
 
@@ -13,7 +14,7 @@ from .utils import (
     generate_id_hex,
     extract_payload
 )
- 
+
 def _parse_update_message(msg: str) -> tuple[int, list]:
     index = find_n_character(msg, ':', 4)
     msg = msg[index + 1:]
@@ -26,19 +27,31 @@ def _parse_update_message(msg: str) -> tuple[int, list]:
     
     return client_id, values
 
-def _gen_client_id(object_id: str) -> int:
-    acc = 0
-    for c in object_id.encode():
-        acc <<= 4
-        acc ^= c    
-        
-    return acc
-
 @dataclass
 class UpdateContext:
     object_id: str
     index: int
     value: Any
+    
+@dataclass(eq=True, unsafe_hash=True)
+class FeatureEntry:
+    object_id: str
+    index: int
+    
+@dataclass
+class ClientPage:
+    client_id: int
+    features: list[FeatureEntry] = field(default_factory=list)
+    modified: bool = False
+    last_mod: float = field(default_factory=time.time)
+    states: list[Any] = field(default_factory=list)
+    
+    def create_payload(self, client_ip, client_port) -> str:
+        features_str = '{' + ','.join([f"{{{fe.object_id},{fe.index}}}" for fe in self.features]) + '}'
+        return f'SYSTEM:clientRegister("{client_ip}",{client_port},{self.client_id},{features_str})'
+    
+    def __hash__(self) -> int:
+        return hash(self.client_id)
 
 class CluClient:
 
@@ -48,14 +61,15 @@ class CluClient:
         port: int,
         cipher: GrentonCipher,
         timeout: float = 1,
-        registration_update_interval: float = 60,
+        client_refresh_interval: float = 60,
         client_ip: str | None = None,
         client_port: int = 0,
-        max_connections: int = 6
+        max_connections: int = 4,
+        page_size: int = 16
     ) -> None:
         self._addr = (ip, port)
         self._timeout = timeout
-        self._registration_update_interval = registration_update_interval
+        self._client_refresh_interval = client_refresh_interval
         
         if client_ip:
             self._local_ip = client_ip
@@ -63,13 +77,14 @@ class CluClient:
             self._local_ip = get_host_ip(ip)
             
         self._cipher = cipher
+        self._page_size = page_size
         
         self._request_semaphore = threading.Semaphore(max_connections)
 
-        self._client_id_map: dict[int, str] = {}
-        self._handler_map: dict[tuple[str, int], Callable[[str, int, Any], None]] = {}
-        self._feature_index_map: dict[str, list[int]] = {}
-        self._states_map: dict[tuple[str, int], Any] = {}
+        self._client_pages_index: dict[FeatureEntry, ClientPage] = {}
+        self._client_pages: dict[int, ClientPage] = {}
+        self._free_client_pages: set[ClientPage] = set()
+        self._handler_map: dict[FeatureEntry, Callable[[UpdateContext], None]] = {}
         
         self._update_receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self._update_receiver_socket.bind((self._local_ip, client_port))
@@ -77,11 +92,10 @@ class CluClient:
         self._update_receiver_thread = threading.Thread(target=self._update_receiver, daemon=True)
         self._update_receiver_thread.start()
         
-        self._client_registration_payloads: dict[str, str] = {}
         self._client_registration_lock = threading.Lock()
-        self._client_registration_thread = threading.Thread(target=self._client_registration, daemon=True)
-        # self._client_registration_thread.start()
-
+        self._client_refresh_thread = threading.Thread(target=self._refresh_client_pages, daemon=True)
+        self._client_refresh_thread.start()
+        
     @property
     def clu_ip(self) -> str:
         return self._addr[0]
@@ -93,11 +107,8 @@ class CluClient:
     @property
     def client_ip(self) -> str:
         return self._local_ip
-    
-    def start_client_registration(self):
-        self._client_registration_thread.start()
 
-    def send_request(self, msg: str) -> str:
+    def send_request(self, msg: str, ignore_response: bool = True) -> str:
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.settimeout(self._timeout)
         
@@ -106,9 +117,10 @@ class CluClient:
         try:
             with self._request_semaphore:
                 sock.sendto(payload, self._addr)
-                resp, _ = sock.recvfrom(1024)
+                if not ignore_response:
+                    resp, _ = sock.recvfrom(1024)
+                    return self._cipher.decrypt(resp).decode()
                 
-            return self._cipher.decrypt(resp).decode()
         except Exception as e:
             raise e
         finally:
@@ -143,45 +155,72 @@ class CluClient:
     async def execute_method_async(self, object_id, index, *args):
         return await asyncio.to_thread(self.execute_method, object_id, index, *args)
     
+    # Client registration section
     def register_value_change_handler(self, object_id: str, index: int, handler: Callable[[UpdateContext], None]) -> None:
         with self._client_registration_lock:
-            client_id = _gen_client_id(object_id)
-            self._client_id_map[client_id] = object_id
+            fentry = FeatureEntry(object_id, index)
+            if fentry in self._handler_map.keys():
+                self._handler_map[fentry] = handler
+                return
             
-            if object_id in self._feature_index_map.keys():
-                self._feature_index_map[object_id].append(index)
+            self._handler_map[fentry] = handler
+            
+            page: ClientPage = None
+            if len(self._free_client_pages) > 0:
+                page = self._free_client_pages.pop()
+                if len(page.features) < self._page_size - 1:
+                    self._free_client_pages.add(page)
+                page.modified = True
+                
             else:
-                self._feature_index_map[object_id] = [index]
+                page = self._create_new_page()
+                
+            page.features.append(fentry)
+            self._client_pages_index[fentry] = page
             
-            self._handler_map[(object_id, index)] = handler
+            self._refresh_page(page)
             
-            payload = self._create_registration_payload(object_id)
-            self._client_registration_payloads[object_id] = payload
-            
-            # resp = self.send_request(payload)
-            # self._handle_update_message(resp)
+    async def register_value_change_handler_async(self, object_id: str, index: int, handler: Callable[[UpdateContext], None]) -> None:
+        await asyncio.to_thread(self.register_value_change_handler, object_id, index, handler)
         
     def remove_value_change_handler(self, object_id: str, index: int) -> None:
         with self._client_registration_lock:
-            self._client_id_map.pop(_gen_client_id(object_id))
+            fentry = FeatureEntry(object_id, index)
+            del self._handler_map[fentry]
             
-            index_map = self._feature_index_map[object_id]
-            index_map.remove(index)
-            if len(index_map) == 0:
-                self._feature_index_map.pop(object_id)
-                self._client_registration_payloads.pop(object_id)
+            page = self._client_pages_index.pop(fentry)
+            page.features.remove(fentry)
+            page.modified = True
+            
+            if len(page.features) == 0:
+                self._free_client_pages.remove(page)
+                self._client_pages.pop(page.client_id)
             else:
-                payload = self._create_registration_payload(object_id)
-                self._client_registration_payloads[object_id] = payload
+                self._free_client_pages.add(page)
+                
+            self._refresh_page(page)
             
-            self._handler_map.pop((object_id, index))
-
-    def _send_lua_request(self, payload) -> str:
+    async def remove_value_change_handler_async(self, object_id: str, index: int) -> None:
+        await asyncio.to_thread(self.remove_value_change_handler(object_id, index))
+        
+    def _send_lua_request(self, payload: str, ignore_response: bool = False, ignore_type: bool = False) -> str:
         req_id = generate_id_hex()
-        payload = f'req:{self._local_ip}:{req_id}:(load("result = {payload} return (type(result) .. \\\":\\\" .. tostring(result))")())' # basically remote code execution
+        
+        if not (ignore_type or ignore_response):
+            # basically remote code execution
+            payload = f'(load("result = {payload} return (type(result) .. \\\":\\\" .. tostring(result))")())'
+            
+        payload = f'req:{self._local_ip}:{req_id}:{payload}' 
     
-        resp = self.send_request(payload)
+        resp = self.send_request(payload, ignore_response)
+        
+        if ignore_response:
+            return None
+        
         resp = extract_payload(resp)
+        
+        if ignore_type:
+            return resp
         
         i = resp.find(":")
         
@@ -196,34 +235,17 @@ class CluClient:
             return value == "true"
         else:
             return None
-
-    def _create_registration_payload(self, object_id: str) -> str:
-        client_id =_gen_client_id(object_id)
-        items = [f"{{{object_id},{i}}}" for i in self._feature_index_map[object_id]]
-        port = self._update_receiver_port
-        payload = f"req:{self._local_ip}:{generate_id_hex()}:SYSTEM:clientRegister(\"{self._local_ip}\",{port},{client_id},{{{','.join(items)}}})"
-        return payload
-    
-    def _client_registration(self) -> None:
         
-        tasks = set()
-        def done_callback(resp_task: asyncio.Task[str]) -> None:
+    def _refresh_client_pages(self):
+        while True:
             with self._client_registration_lock:
-                self._handle_update_message(resp_task.result())
-            tasks.discard(resp_task)
-        
-        async def main():
-            while True:
-                # TODO: I'm not sure if that Lock is a good idea
-                with self._client_registration_lock:
-                    for payload in self._client_registration_payloads.values():
-                        task = asyncio.create_task(self.send_request_async(payload))
-                        tasks.add(task)
-                        task.add_done_callback(done_callback)
-                
-                await asyncio.sleep(self._registration_update_interval)
-                
-        asyncio.run(main())
+                try:
+                    for page in self._client_pages.values():
+                        self._refresh_page(page)
+                except IOError:
+                    pass
+            
+            time.sleep(self._client_refresh_interval)
         
     def _update_receiver(self) -> None:
         
@@ -234,26 +256,59 @@ class CluClient:
             except:
                 continue
             
+            msg_time = time.time()
             with self._client_registration_lock:
-                self._handle_update_message(decrypted)
+                self._handle_update_message(decrypted, msg_time)
             
-    def _handle_update_message(self, message: str):
-        
+    def _handle_update_message(self, message: str, message_timestamp: float):
         client_id, values = _parse_update_message(message)
-        object_id = self._client_id_map.get(client_id, None)
-        if object_id == None:
+        page = self._client_pages.get(client_id, None)
+        if page == None:
             return
         
-        index_map = self._feature_index_map[object_id]
-        
-        for i, v in enumerate(values):
-            index = index_map[i]
+        if page.modified:
+            page.states = values
+            page.modified = False
             
-            key = (object_id, index)
+            if page.last_mod > message_timestamp:
+                return
             
-            prev_state = self._states_map.get(key, None)
-            if prev_state != v:
-                self._states_map[key] = v
-                handler = self._handler_map[key]
+            for entry, value in zip(page.features, values):
+                self._procces_update(entry, value)
                 
-                threading.Thread(target=handler, args=(UpdateContext(object_id, index, v),)).start()
+        else:
+            for entry, _, new_value in filter(lambda x: x[1] != x[2], zip(page.features, page.states, values)):
+                self._procces_update(entry, new_value)
+            page.states = values
+                
+    def _procces_update(self, entry: FeatureEntry, value: Any):
+        handler = self._handler_map[entry]
+        threading.Thread(target=handler, args=(UpdateContext(entry.object_id, entry.index, value),)).start()
+                
+    def _create_new_page(self) -> ClientPage:
+        used_ids = self._client_pages.keys()
+        
+        client_id = 1
+        while client_id in used_ids:
+            client_id += 1
+            
+        page = ClientPage(client_id)
+        
+        self._client_pages[client_id] = page
+        self._free_client_pages.add(page)
+        
+        return page
+    
+    def _register_page(self, page: ClientPage) -> None:
+        resp = self._send_lua_request(page.create_payload(self._local_ip, self._update_receiver_port), ignore_type=True)
+        i = resp.find(':')
+        self._handle_update_message(resp[i+1:], time.time())
+    
+    def _unregister_page(self, page: ClientPage) -> None:
+        self._send_lua_request(f'SYSTEM:clientDestroy("{self._local_ip}",{self._update_receiver_port},{page.client_id})', ignore_response=True)
+        
+    def _refresh_page(self, page: ClientPage) -> None:
+        self._unregister_page(page)
+        self._register_page(page)
+        page.last_mod = time.time()
+        
